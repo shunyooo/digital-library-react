9e38844a90d8d001c1cad04b3d0181e611167f75
Syunyo Kawamoto <syunyo1028@yahoo.co.jp>
2020-03-29 03:06:43 +0900

    add antd, react-router-dom

---
+++ README.md
@@ -0,0 +1,21 @@
+# Digital Library
+
+できるだけ無料で回すためのライブラリ
+
+- ログイン機能
+- コミック管理
+    - アップロード, ダウンロード
+    - 一覧表示
+    - 検索
+    - 最新刊更新検知
+
+
+
+## React Library
+
+- [Ant Design](https://ant.design/)
+    - Alibabaが作っているCSSフレームワーク
+    - [人気](https://www.npmtrends.com/antd-vs-material-ui-vs-rebass-vs-semantic-ui-react-vs-uikit)っぽいので採用
+
+
+
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+// 使うコンポーネントをインポート
+import { Layout, Button, Typography, Row, Col } from 'antd'; 
 import './App.css';
+
+// 一部の要素は各コンポーネントを元に定義する必要がある
+// 詳しくはドキュメントのソースコードを見ればわかる
+const { Content } = Layout;
+const { Title } = Typography;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout style={{textAlign: "center" }}>
+        <Content style={{ margin: "60px 16px" }}>
+          <Title>Digital Library React</Title>
+          <Row style={{ margin: "60px 16px" }}>
+            <Col xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
+              <Title level={3}>セットアップ</Title>
+              <Button type="primary">Link</Button>
+            </Col>
+            <Col xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
+              <Title level={3}>試してみる</Title>
+              <Button type="danger">Link</Button>
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
     </div>
   );
 }
