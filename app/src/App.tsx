395def6a67013e5f1c28ff1744334f808d364589
Syunyo Kawamoto <syunyo1028@yahoo.co.jp>
2020-05-05 08:19:08 +0900

    remove antd, add material-ui

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,31 +1,10 @@
 import React from 'react';
-// 使うコンポーネントをインポート
-import { Layout, Button, Typography, Row, Col } from 'antd'; 
 import './App.css';
-
-// 一部の要素は各コンポーネントを元に定義する必要がある
-// 詳しくはドキュメントのソースコードを見ればわかる
-const { Content } = Layout;
-const { Title } = Typography;
 
 function App() {
   return (
     <div className="App">
-      <Layout style={{textAlign: "center" }}>
-        <Content style={{ margin: "60px 16px" }}>
-          <Title>Digital Library React</Title>
-          <Row style={{ margin: "60px 16px" }}>
-            <Col xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
-              <Title level={3}>セットアップ</Title>
-              <Button type="primary">Link</Button>
-            </Col>
-            <Col xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
-              <Title level={3}>試してみる</Title>
-              <Button type="danger">Link</Button>
-            </Col>
-          </Row>
-        </Content>
-      </Layout>
+      React Digital Library
     </div>
   );
 }
