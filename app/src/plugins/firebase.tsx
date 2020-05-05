7df10215b84a480d3afc92a2ff07c975f1378fd8
Syunyo Kawamoto <syunyo1028@yahoo.co.jp>
2020-05-05 20:29:33 +0900

    add firebase settings

--- app/src/plugins/firebase.tsx
+++ app/src/plugins/firebase.tsx
@@ -1,19 +1,21 @@
 import firebase from 'firebase';
 
-const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_DEV_FIREBASE_APP_API_KEY,
+  authDomain: process.env.REACT_APP_DEV_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DEV_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_DEV_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_DEV_FIREBASE_APP_ID
 };
 
 if (!firebase.apps.length) {
-    console.log('initialize firebase.', config);
-    firebase.initializeApp(config);
+  console.log('initialize firebase.', firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 
 export default firebase;
 export const auth = firebase.auth();
 export const database = firebase.database();
+export const storage = firebase.storage();
