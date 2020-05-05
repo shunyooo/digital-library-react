712230c5efc34bc7568abac10c0e4f4ac4d8f171
Syunyo Kawamoto <syunyo1028@yahoo.co.jp>
2020-05-05 21:24:18 +0900

    re-init by react-webpack-typescript-starter

---
+++ app/configs/webpack/prod.js
@@ -0,0 +1,20 @@
+// production config
+const merge = require('webpack-merge');
+const {resolve} = require('path');
+
+const commonConfig = require('./common');
+
+module.exports = merge(commonConfig, {
+  mode: 'production',
+  entry: [
+    `webpack-dev-server/client?http://localhost:${process.env.REACT_APP_PRD_PORT}`,
+    './index.tsx'
+  ],
+  output: {
+    filename: 'js/bundle.[hash].min.js',
+    path: resolve(__dirname, '../../dist'),
+    publicPath: '/',
+  },
+  devtool: 'source-map',
+  plugins: [],
+});
